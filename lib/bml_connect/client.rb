03172ade6bad5b6f6543a336b1570380f5c20e03
icernn <ishan@oxiqa.com>
2021-09-15 23:05:37 +0500

    refactor: updated http client to handle json requests

--- lib/bml_connect/client.rb
+++ lib/bml_connect/client.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'faraday'
+require 'faraday_middleware'
 require 'deep_merge/rails_compat'
 
 module BMLConnect
@@ -13,9 +14,10 @@ module BMLConnect
 
     attr_reader(:api_key, :http_client, :transactions)
 
-    def initialize(api_key: nil, app_id: nil, mode: "production", options: {})
+    def initialize(api_key: nil, app_id: nil, mode: nil, options: {})
       @api_key = api_key || (defined?(BML_API_KEY) ? BML_API_KEY : 'not-set')
       @app_id = app_id || (defined?(BML_APP_ID) ? BML_APP_ID : 'not-set')
+      @mode = mode || (defined?(BML_MODE) ? BML_MODE : 'production')
       @mode = mode
       @http_client = initialize_http_client(options)
       @transactions = Transactions.new(self)
@@ -35,7 +37,7 @@ module BMLConnect
         'appVersion' => BML_APP_VERSION,
         'signMethod' => BML_SIGN_METHOD,
       })
-      @http_client.post(action, data)
+      @http_client.post(action, data.to_json)
     end
 
     def get(action, params = {})
@@ -52,7 +54,10 @@ module BMLConnect
           'Authorization' =>  api_key,
         }
       };
-      Faraday.new(defaults.deeper_merge!(options))
+      Faraday.new(defaults.deeper_merge!(options)) do |f|
+        f.response :logger, nil, { headers: true, bodies: true }
+        f.response :json
+      end
     end
   end
 end
